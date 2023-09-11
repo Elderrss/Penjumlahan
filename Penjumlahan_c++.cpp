c812fda777315bdccea6b879fc44dc30fa4d12f2
Elderrss <144640867+Elderrss@users.noreply.github.com>
2023-09-11 09:22:39 +0700

    Create Penjumlahan_c++.cpp

---
+++ Penjumlahan_c++.cpp
@@ -0,0 +1,13 @@
+#include<iostream> 
+using namespace  std;
+
+int main ( ){
+      int a, b, c ;
+      cout<<"masukan angka pertama: ";
+      cin>>a ;
+      cout<< "masukan angka kedua: ";
+      cin>>b ;
+      c= a*b ;
+      cout<<"hasilnya adalah: "<<c<<endl;
+      return 0;
+}
